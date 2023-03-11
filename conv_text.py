84d041b27f06ba96ffd409ca38936c178925eb59
Keith Adams <kma@pebblebed.com>
2023-03-11 11:27:29 -0800

    Fix some problems with size estimation.

--- conv_text.py
+++ conv_text.py
@@ -8,26 +8,24 @@ class FilterBank(nn.Module):
     def __init__(self,
                  channels_in,
                  channels_out,
-                 filter_width,
                  filter_depth):
+        self.channels_in = channels_in
         self.channels_out = channels_out
-        self.filter_width = filter_width
         self.filter_depth = filter_depth
         
         super(FilterBank, self).__init__()
         self.f = nn.Sequential(
-          nn.Conv2d(channels_in, channels_out, (filter_width, filter_depth)),
+          nn.Conv1d(channels_in, channels_out, filter_depth),
           nn.MaxPool2d((1, 2)),
           nn.Dropout(),
           nn.ReLU())
 
-    def input_size(self, input_width, input_length):
-        return (1, input_width, input_length)
+    def input_size(self, input_length):
+        return (self.channels_in, input_length)
 
-    def output_size(self, input_width, input_length):
+    def output_size(self, input_length):
         return (self.channels_out,
-                1 + (input_width - self.filter_width),
-                int((input_length - self.filter_depth + 1) / 2))
+               int((1 + input_length - self.filter_depth) / 2))
     
     def forward(self, x):
         return self.f(x)
@@ -35,23 +33,24 @@ class FilterBank(nn.Module):
 class FilterApparatus(nn.Module):
     def __init__(self, embedding_width):
         super(FilterApparatus, self).__init__()
-        self.fb1 = FilterBank(1, 1024, embedding_width, 15)
-        self.fb2 = FilterBank(1024, 512, 1, 15)
-        self.fb3 = FilterBank(512, 128, 1, 15)
-        self.fb4 = FilterBank(128, 64, 1, 15)
-        self.fb5 = FilterBank(64, 64, 1, 15)
+        self.fb1 = FilterBank(embedding_width, 1024, 15)
+        self.fb2 = FilterBank(1024, 512, 15)
+        self.fb3 = FilterBank(512, 256, 15)
+        self.fb4 = FilterBank(256, 128, 15)
+        self.fb5 = FilterBank(128, 64, 15)
         self.apparatus = nn.Sequential(
             self.fb1, self.fb2, self.fb3, self.fb4, self.fb5)
 
     def input_size(self, input_width, input_length):
         return self.fb1.input_size(input_width, input_length)
                                    
-    def output_size(self, input_width, input_length):
-        fb1_sz = self.fb1.output_size(input_width, input_length)
-        fb2_sz = self.fb2.output_size(fb1_sz[1], fb1_sz[2])
-        fb3_sz = self.fb3.output_size(fb2_sz[1], fb2_sz[2])
-        fb4_sz = self.fb4.output_size(fb3_sz[1], fb3_sz[2])
-        return self.fb5.output_size(fb4_sz[1], fb4_sz[2])
+    def output_size(self, input_length):
+        import pdb; pdb.set_trace()
+        fb1_sz = self.fb1.output_size(input_length)
+        fb2_sz = self.fb2.output_size(fb1_sz[1])
+        fb3_sz = self.fb3.output_size(fb2_sz[1])
+        fb4_sz = self.fb4.output_size(fb3_sz[1])
+        return self.fb5.output_size(fb4_sz[1])
     
     def forward(self, x):
         if False:
@@ -73,8 +72,10 @@ class ConvText(nn.Module):
         self.embedding_width = embedding_width
         self.token_embedding_table = nn.Embedding(vocab_size, embedding_width).to(dev())
         self.filter_stack = FilterApparatus(embedding_width)
-        filter_out_size = self.filter_stack.output_size(embedding_width, context_size)
-        filter_out_nparams = filter_out_size[0] * filter_out_size[1] * filter_out_size[2]
+        import pdb; pdb.set_trace()
+        filter_out_size = self.filter_stack.output_size(context_size)
+        filter_out_nparams = filter_out_size[0] * filter_out_size[1]
+        print("first FC has {} inputs ({})".format(filter_out_nparams, filter_out_size))
         self.model = nn.Sequential(
             self.filter_stack,
             nn.Flatten(),
@@ -91,12 +92,13 @@ class ConvText(nn.Module):
         padded_idx = torch.zeros((idx.shape[0], self.context_length), dtype=torch.long ).to(dev())
         padded_idx[:, -idx.shape[1]:] = idx
         assert(padded_idx.shape[1] == self.context_length)
-        # input is sparse character indices, shaped (B,T,C)
-        projected = self.token_embedding_table(padded_idx)
+        # embedding table produces dense, embedding per token, shaped (B,T,C). Conv
+        # layer expects (B,C,T), so:
+        projected = self.token_embedding_table(padded_idx).transpose(1,2)
         assert len(projected.size()) == 3
         assert projected.size()[0] == idx.size()[0] # B
-        assert projected.size()[1] == self.context_length # T
-        assert projected.size()[2] == self.embedding_width # C
+        assert projected.size()[1] == self.embedding_width # C
+        assert projected.size()[2] == self.context_length # T
         
         logits = self.model(projected)
 
