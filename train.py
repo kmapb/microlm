e1c7365e9fb73ea443a33c2c12a29f0feb258881
Keith Adams <kma@pebblebed.com>
2023-03-25 19:46:29 -0700

    Some basic cleanups now that we are clear what we are doing.

--- train.py
+++ train.py
@@ -10,25 +10,27 @@ if __name__ == "__main__":
     import sys
     CFG= {
         'model': 'conv_text',
-        'dataset': 'the_pile',
-        'dataset_cfg': 'all',
+        'dataset': 'bookcorpus',
+        'dataset_cfg': 'plain_text',
         'fname' : 'model-conv-text',
-        'embed_width': 512,
+        'embed_width': 2048,
         'filter_height': 5,
     }
+    
+    # Compute the width of the first fully-connected layer; needs to be
+    # big enough to accept the output of the convolutional layer.
     CFG['context_width'] = (CFG['filter_height'] + 1) * CFG['embed_width']
 
-    print("convo!!!")
+    # Allow the hardware to use mixed precision
     torch.set_float32_matmul_precision('medium')
-    # dataset = { 'name': 'wikitext', 'config': 'wikitext-103-raw-v1', 'streaming': False}
-    # dataset = { 'name': 'bookcorpus', 'config': 'plain_text', 'streaming': False}
-    dataset = { 'name': 'wikitext', 'config': 'wikitext-2-raw-v1', 'streaming': False}
-    # set = { 'name': 'the_pile', 'config': 'all', 'streaming': True}
-    model = ReConvText(text_data.vocabulary_size(), CFG['filter_height'],
-                       CFG['embed_width'], CFG['context_width'])
+
+    model = ReConvText(text_data.vocabulary_size(),
+                       CFG['filter_height'],
+                       CFG['embed_width'],
+                       CFG['context_width'])
     
     pl.seed_everything(71177)
-    dm = text_data.TextDataModule(dataset['name'], dataset['config'], streaming=dataset['streaming'])
+    dm = text_data.TextDataModule(CFG['dataset'], CFG['dataset_cfg'], streaming=False)
     # saves top-K checkpoints based on "val_loss" metric
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         save_top_k=10,
@@ -40,18 +42,12 @@ if __name__ == "__main__":
 
     trainer = pl.Trainer(accelerator='auto',
                          devices='auto',
-                         max_time={'hours': 4},
+                         max_time={'hours': 48},
                          callbacks=[checkpoint_callback],
-                         # overfit_batches=100,
-                         # log_every_n_steps=1,
-                         # limit_val_batches=10,
-                         # limit_train_batches=1, # Overfit?
-                         max_epochs=2,
-                         # deterministic=True,
-                         # gradient_clip_val=0.5
+                         val_check_interval=0.25,
                          )
 
 
     trainer.fit(model, dm)
     trainer.save_checkpoint('model.ckpt')
-    # trainer.test(model, dm)
+    trainer.test(model, dm)
